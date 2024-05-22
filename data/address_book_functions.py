bb1b2547900b4afb6e66e208250b32f0410cad55
Vademandr <vademandrpro@gmail.com>
2024-05-22 21:10:01 +0300

    Update AddressBook1

--- data/address_book_functions.py
+++ data/address_book_functions.py
@@ -1,5 +1,5 @@
 import datetime
-from data.classes import AddressBook, Birthday, DateFormatError
+from data.classes import AddressBook, Birthday, DateFormatError, TypeEmailError, RecordNotFindError, PhoneNotFindError, Record, LenPhoneError, TypePhoneError
 
 def upcoming_birthdays(book, args):
     """
@@ -79,3 +79,232 @@ def del_birthday(book:AddressBook, args:list):
             print(f'Contact id {args[0]} not found')
     else:
         print('Error: Invalid command format.')
+
+def address_record(book:AddressBook, args:list):
+    """
+    Додати адресу до запису в AddressBook
+    Args:
+        book (AddressBook): Екземпляр AddressBook, що містить контактну інформацію.
+        user id (int): ID користувача в AddressBook
+        address (str): адреса користувача 
+    """
+    if len(args) >=2:
+        if int(args[0]) in book.data:
+            rec = book.data[int(args[0])]
+            rec.address = ' '.join(s for s in args[1:])
+            print('Address added sucessfully.')
+        else:
+            print(f'Contact id {args[0]} not found')
+    else:
+        print('Error: Invalid command.')
+
+def del_address(book:AddressBook, args:list):
+    """
+    Видалення адресу з запису в AddressBook
+    Args:
+        book (AddressBook): Екземпляр AddressBook, що містить контактну інформацію.
+        user id (int): ID користувача в AddressBook
+    """
+    if len(args) ==1:
+        if int(args[0]) in book.data:
+            rec = book.data[int(args[0])]
+            rec.address = ''
+            print('Address deleted sucessfully.')
+        else:
+            print(f'Contact id {args[0]} not found')
+    else:
+        print('Error: Invalid command.')
+
+def add_email_in_rec(book:AddressBook, args:list):
+    """
+    Додати електронну пошту до запису в AddressBook
+    Args:
+        book (AddressBook): Екземпляр AddressBook, що містить контактну інформацію.
+        user id (int): ID користувача в AddressBook
+        email (Email): електронна пошта користувача 
+    """
+    if len(args) ==2:
+        try:
+            if int(args[0]) in book.data:
+                rec = book.data[int(args[0])]
+                rec.add_email(args[1])
+                print('Email added sucessfully.')
+            else:
+                print(f'Contact id {args[0]} not found')
+        except TypeEmailError:
+            print('Error: Wrong email format')
+    else:
+        print('Error: Invalid command format.')
+
+def edit_email_in_rec(book:AddressBook, args:list):
+    """
+    Редагувати електронну пошту для запису в AddressBook
+    Args:
+        book (AddressBook): Екземпляр AddressBook, що містить контактну інформацію.
+        user id (int): ID користувача в AddressBook
+        email (Email): електронна пошта користувача 
+    """
+    if len(args) ==3:
+        try:
+            rec = book.data[int(args[0])]
+            rec.edit_email(args[1], args[2])
+            print('Email changed sucessfully.')
+        except RecordNotFindError:
+            print('Error: User not found.')
+        except PhoneNotFindError:
+            print('Error: Email to change is not found.')
+        except TypeEmailError:
+            print('Error: Wrong email format')    
+    else:
+        print('Error: Invalid command format.')
+
+def del_email_in_rec(book:AddressBook, args:list):
+    """
+    Видалити електронну пошту для запису AddressBook
+    Args:
+        book (AddressBook): Екземпляр AddressBook, що містить контактну інформацію.
+        user id (int): ID користувача в AddressBook
+        email (Email): електронна пошта користувача 
+    """
+    if len(args) ==2:
+        try:
+            rec = book.data[int(args[0])]
+            rec.remove_email(args[1])
+            print('Email deleted sucessfully.')
+        except RecordNotFindError:
+            print('Error: User not found.')
+        except PhoneNotFindError:
+            print('Error: Email to delete is not found.')
+    else:
+        print('Error: Invalid command format.')
+
+def add_record(book:AddressBook, args:list):
+    """
+    Додавання нового запису в AddressBook
+    Args:
+        book (AddressBook): Екземпляр AddressBook, що містить контактну інформацію.
+        name (Name): ім'я користувача 
+    """
+    if len(args) ==1:
+        book.add_record(Record(args[0], book))
+        print('Record created sucessfully')
+    else:
+        print('Error: Invalid command format.')
+
+def edit_record(book:AddressBook, args:list):
+    """
+    Редагувати ім'я у записі в AddressBook
+    Args:
+        book (AddressBook): Екземпляр AddressBook, що містить контактну інформацію.
+        user id (int): ID користувача в AddressBook
+        name (Name): нове ім'я користувача 
+    """
+    if len(args) ==2:
+        if int(args[0]) in book.data:
+            book.edit_record(args)
+            print('Record sucessfully deleted')
+        else:
+            print(f'Contact id {args[0]} not found')
+    else:
+        print('Error: Invalid command format.')
+
+def del_record(book:AddressBook, args:list):
+    """
+    Видалити запис в AddressBook
+    Args:
+        book (AddressBook): Екземпляр AddressBook, що містить контактну інформацію.
+        user id (int): ID користувача в AddressBook
+    """
+    if len(args) ==1:
+        if int(args[0]) in book.data:
+            book.del_record(args)
+            print('Record sucessfully deleted')
+        else:
+            print(f'Contact id {args[0]} not found')
+    else:
+        print('Error: Invalid command format.')
+
+def add_phone_in_rec(book:AddressBook, args:list):
+    """
+    Додати телефон до запису в AddressBook
+    Args:
+        book (AddressBook): Екземпляр AddressBook, що містить контактну інформацію.
+        user id (int): ID користувача в AddressBook
+        phone (str): номер телефону для додавання
+    """
+    if len(args) ==2:
+        try:
+            if int(args[0]) in book.data:
+                rec = book.data[int(args[0])]
+                rec.add_phone(args[1])
+                print('Phone added sucessfully.')
+            else:
+                print(f'Contact id {args[0]} not found')
+        except LenPhoneError:
+            print('Error: Phone must be 10 symbols')
+        except TypePhoneError:
+            print('Error: Phone must be 10 symbols')
+    else:
+        print('Error: Invalid command format.')
+
+def edit_phone_in_rec(book:AddressBook, args:list):
+    """
+    Замінити старий телефон на новий запис в AddressBook
+    Args:
+        book (AddressBook): Екземпляр AddressBook, що містить контактну інформацію.
+        user id (int): ID користувача в AddressBook
+        old phone (str): номер телефону для зміни
+        new phone (str): новий номер телефону
+    """
+    if len(args) ==3:
+        try:
+            rec = book.data[int(args[0])]
+            rec.edit_phone(args[1], args[2])
+            print('Phone changed sucessfully.')
+        except RecordNotFindError:
+            print('Error: User not found.')
+        except LenPhoneError:
+            print('Error: Phone must be 10 symbols')
+        except PhoneNotFindError:
+            print('Error: Phone to change is not found.')
+        except TypePhoneError:
+            print('Error: Phone must consist from digits')
+    else:
+        print('Error: Invalid command format.')
+
+def del_phone_in_rec(book:AddressBook, args:list):
+    """
+    Видалити телефон з запису в AddressBook
+    Args:
+        book (AddressBook): Екземпляр AddressBook, що містить контактну інформацію.
+        user id (int): ID користувача в AddressBook
+        phone (str): номер телефону для видалення
+    """
+    if len(args) ==2:
+        try:
+            rec = book.data[int(args[0])]
+            rec.remove_phone(args[1])
+            print('Phone deleted sucessfully.')
+        except RecordNotFindError:
+            print('Error: User not found.')
+        except PhoneNotFindError:
+            print('Error: Phone to delete is not found.')
+    else:
+        print('Error: Invalid command format.')
+
+def find_in_records(book:AddressBook, args:list):
+    """
+    Пошук в AddressBook
+    Args:
+        search string (str): рядок для пошуку в полях адресної книги: Name, Adress, Phones, Emails, Birthday
+    """
+    if len(args) ==1 and len(args[0]) > 1:
+        count = 0
+        sstring = args[0].lower()
+        for _, record in book.data.items():
+            if record.searchstring().find(sstring) >=0:
+                print(record)
+                count += 1
+        print(f'Search complete. Total {count} records found.')    
+    else:
+        print('Error: Invalid command format. Search string must be more then 2 symbols')
